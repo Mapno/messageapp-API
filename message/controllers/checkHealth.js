05e1705e6f131fe633d50354526bbf11a1e04caa
Mapno <mariorvinas@gmail.com>
2018-11-12 16:46:51 +0100

    redirected health checks to /health endpoint

--- message/controllers/checkHealth.js
+++ message/controllers/checkHealth.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT;
 
 module.exports = function(req, res, next) {
-    return res.status(200).send(`Status: Health. Container: ${port}`);
+    return Promise.resolve(res.status(200)).then(() => console.log(`Health checked on port: ${port}. Status: healthy.`));
 };
