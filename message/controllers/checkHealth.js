99af69dbf5bddd6a0749a1daa339259c1acdbcdd
Mapno <mariorvinas@gmail.com>
2018-11-13 15:51:43 +0100

    added config files to launch services with a service discovery

--- message/controllers/checkHealth.js
+++ message/controllers/checkHealth.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT;
 
 module.exports = function(req, res, next) {
-    return Promise.resolve(res.status(200)).then(() => console.log(`Health checked on port: ${port}. Status: healthy.`));
+    return res.status(200)
 };
